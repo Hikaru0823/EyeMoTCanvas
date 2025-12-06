5522648ddfea577fc1553185c0890ddb7637209f
Hikaru0823 <167277890+Hikaru0823@users.noreply.github.com>
2025-12-06 09:26:58 +0900

    テストマネージャー追加

---
+++ Assets/Scripts/TestManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ゲーム開始時に呼び出す
+        Debug.Log("TestManager started.");
+    }
+}
